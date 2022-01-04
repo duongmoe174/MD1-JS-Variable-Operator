e539b7408b210f1c7ebc7c1667173187bb4c820f
duongmoe174 <duongmoe174@gmail.com>
2022-01-05 00:03:59 +0700

    thuc hanh khai bao bien va toan tu

---
+++ operator.js
@@ -0,0 +1,7 @@
+let inputWidth = prompt("Nhập chiều rộng");
+let inputHeight = prompt("Nhập chiều cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
+
